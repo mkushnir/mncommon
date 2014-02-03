90acced83cf88b3ffc08d4c6fd09982c7262d1d9
Markiyan Kushnir [at home] <markiyan.kushnir@gmail.com>
2014-02-03 22:24:44 +0200

    fasthash moved from mrkapp to here.

---
+++ src/fasthash.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <mrkapp.h>
+#include <mrkcommon/dumpm.h>
+
+static inline uint64_t
+rotl64(uint64_t n, int b)
+{
+    return (n << b) | (n >> (64 - b));
+}
+
+static inline uint64_t
+rotr64(uint64_t n, int b)
+{
+    return (n >> b) | (n << (64 - b));
+}
+
+uint64_t
+fasthash(uint64_t n, const unsigned char *s, size_t sz)
+{
+    size_t mod = sz % sizeof(uint64_t);
+    size_t i, j, sz1;
+
+    sz1 = sz - mod;
+
+#define C(f) \
+    for (i = 0, j = 0;                      \
+         j < sz1;                           \
+         ++i, j += sizeof(uint64_t)) {      \
+        n += *((uint64_t *)(s + j));        \
+        n = f(n, (s[j] + i) % 8);           \
+    }                                       \
+    if (mod) {                              \
+        union {                             \
+            uint64_t u64;                   \
+            char  c[sizeof(uint64_t)];      \
+        } ss;                               \
+        ss.u64 = 0UL;                       \
+        for (i = 0; i < mod; ++i) {         \
+            ss.c[i] = s[sz - 1 - i];        \
+        }                                   \
+        n += ss.u64;                        \
+        n = f(n, (ss.c[0] + i) % 8);        \
+    }
+
+    if (sz < sizeof(uint64_t) * 2) {
+        C(rotl64);
+    } else {
+        C(rotr64);
+    }
+    return n;
+}
+
---
+++ src/mrkcommon/fasthash.h
@@ -0,0 +1,16 @@
+#ifndef MRKCOMMON_FASTHASH_H_DEFINED
+#define MRKCOMMON_FASTHASH_H_DEFINED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+uint64_t fasthash(uint64_t, const unsigned char *, size_t);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* MRKCOMMON_FASTHASH_H_DEFINED */
+
