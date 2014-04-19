06d94740637e37607878c3387cba173a0607447b
Markiyan Kushnir [at home] <markiyan.kushnir@gmail.com>
2014-04-19 15:40:35 +0300

    missing files

---
+++ src/test/testdict.c
@@ -0,0 +1,93 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "unittest.h"
+
+#include "mrkcommon/dict.h"
+#include "mrkcommon/dumpm.h"
+#include "mrkcommon/util.h"
+
+static uint64_t
+myhash(UNUSED void *key)
+{
+    return (uint64_t)key;
+}
+
+static int
+myfini(void *key, void *value)
+{
+    TRACE("key=%p value=%p", key, value);
+    return 0;
+}
+
+static int
+mycmp(void *a, void *b)
+{
+    return (int)((intptr_t)a - (intptr_t)b);
+}
+
+static int
+mycb(void *a, void *b, UNUSED void *udata)
+{
+    TRACE("%p:%p", a, b);
+    return 0;
+}
+
+static void
+test0(void)
+{
+    struct {
+        long rnd;
+        int in;
+        int expected;
+    } data[] = {
+        {0, 0, 0},
+    };
+    dict_t dict;
+    void *v1, *v2;
+
+
+    UNITTEST_PROLOG_RAND;
+
+    FOREACHDATA {
+        TRACE("in=%d expected=%d", CDATA.in, CDATA.expected);
+        assert(CDATA.in == CDATA.expected);
+    }
+
+    dict_init(&dict, 7, myhash, mycmp, myfini);
+
+    dict_set_item(&dict, (void *)1, (void *)10);
+    dict_traverse(&dict, mycb, NULL);
+
+    dict_set_item(&dict, (void *)1, (void *)10);
+    dict_set_item(&dict, (void *)1, (void *)10);
+    dict_set_item(&dict, (void *)1, (void *)10);
+    dict_set_item(&dict, (void *)1, (void *)10);
+    dict_traverse(&dict, mycb, NULL);
+
+    v1 = dict_get_item(&dict, (void *)1);
+    TRACE("v1=%p", v1);
+
+    v2 = dict_remove_item(&dict, (void *)1);
+    dict_traverse(&dict, mycb, NULL);
+    TRACE("v2=%p", v2);
+    v2 = dict_get_item(&dict, (void *)1);
+    TRACE("v2=%p", v2);
+
+    for (i = 0; i < 21; ++i) {
+        dict_set_item(&dict, (void *)(uintptr_t)i, (void *)(uintptr_t)i);
+    }
+    dict_traverse(&dict, mycb, NULL);
+
+    dict_fini(&dict);
+}
+
+int
+main(void)
+{
+    test0();
+    return 0;
+}
+
+
---
+++ src/test/testfasthash.c
@@ -0,0 +1,80 @@
+#include <assert.h>
+#include <stdlib.h>
+#define _WITH_GETLINE
+#include <stdio.h>
+#include <time.h>
+
+#include "diag.h"
+#include "unittest.h"
+#include <mrkcommon/dumpm.h>
+#include <mrkcommon/trie.h>
+#include <mrkcommon/util.h>
+#include <mrkcommon/fasthash.h>
+
+#ifndef NDEBUG
+const char *_malloc_options = "AJ";
+#endif
+
+static int
+trie_node_fini(trie_node_t *trn, UNUSED uint64_t key, UNUSED void *udata)
+{
+    if (trn->value != NULL) {
+        free(trn->value);
+        trn->value = NULL;
+    }
+    return 0;
+}
+
+static void
+test0(const char *fname)
+{
+    FILE *f;
+    trie_t tr;
+    char *buf;
+    size_t bufsz;
+    ssize_t sz;
+
+    assert(fname != NULL);
+
+    if ((f = fopen(fname, "r")) == NULL) {
+        FAIL("fopen");
+    }
+    trie_init(&tr);
+
+    for (buf = NULL;
+         (sz = getline(&buf, &bufsz, f)) != -1;
+         buf = NULL) {
+
+        uint64_t hash;
+        trie_node_t *trn;
+
+        buf[sz - 1] = '\0';
+        --sz;
+
+        hash = fasthash(0, (unsigned char *)buf, sz);
+        printf("%016lx %s\n", hash, buf);
+        if ((trn = trie_find_exact(&tr, hash)) != NULL) {
+            TRACE("collision %016lx %s %s", hash, buf, (char *)(trn->value));
+            free(buf);
+        } else {
+            trn = trie_add_node(&tr, hash);
+            trn->value = buf;
+        }
+    }
+
+    fclose(f);
+    trie_traverse(&tr, trie_node_fini, NULL);
+    trie_fini(&tr);
+
+}
+
+int
+main(int argc, char **argv)
+{
+    char *fname = NULL;
+    if (argc >= 2) {
+        fname = argv[1];
+    }
+    test0(fname);
+    return 0;
+}
