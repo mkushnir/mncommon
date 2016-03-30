e8e5cc9702519ead9fbf679705c11ba2cb7fd8df
Markiyan Kushnir <markiyan.kushnir@gmail.com>
2016-03-30 11:17:58 +0300

    fixed bytestream_read_more aux

--- src/bytestream_aux.c
+++ src/bytestream_aux.c
@@ -1,12 +1,21 @@
 #include <mrkcommon/bytestream.h>
 #include <mrkcommon/bytes.h>
+#include <mrkcommon/util.h>
 
 static ssize_t
 bytestream_read_more_bytes(bytestream_t *bs, UNUSED int fd, ssize_t sz)
 {
-    ssize_t need;
-    need = (bs->eod + sz) - bs->buf.sz;
-    if (need > 0) {
+    ssize_t over;
+    ssize_t nleft;
+
+    nleft = bs->buf.sz - bs->eod;
+
+    if (nleft <= 0) {
+        return -1;
+    }
+    sz = MIN(sz, nleft);
+    over = (bs->eod + sz) - bs->buf.sz;
+    if (over > 0) {
         return -1;
     }
     bs->eod += sz;
