1a1098de4b7a04ae3a1d557a1aaef61bb9f76325
Markiyan Kushnir [at home] <markiyan.kushnir@gmail.com>
2016-05-05 01:55:38 +0300

    internal rename in stqueue and dtqueue

--- src/mrkcommon/dtqueue.h
+++ src/mrkcommon/dtqueue.h
@@ -10,25 +10,25 @@
 extern "C" {
 #endif
 
-#define DTQUEUE(ty, n) \
-struct {               \
-    struct ty *head;   \
-    struct ty *tail;   \
-    size_t nelems;     \
-} n                    \
+#define DTQUEUE(ty, n)         \
+struct {                       \
+    struct ty *dtq_head;       \
+    struct ty *dtq_tail;       \
+    size_t nelems;             \
+} n                            \
 
 
 #define DTQUEUE_ENTRY(ty, n)   \
 struct {                       \
-    struct ty *next;           \
-    struct ty *prev;           \
+    struct ty *dtq_next;       \
+    struct ty *dtq_prev;       \
 } n                            \
 
 
 #define DTQUEUE_INIT(q)        \
     do {                       \
-        (q)->head = NULL;      \
-        (q)->tail = NULL;      \
+        (q)->dtq_head = NULL;  \
+        (q)->dtq_tail = NULL;  \
         (q)->nelems = 0;       \
     } while (0)                \
 
@@ -37,54 +37,54 @@ struct {                       \
 
 #define DTQUEUE_ENTRY_INIT(link, entry)\
     do {                               \
-        (entry)->link.next = NULL;     \
-        (entry)->link.prev = NULL;     \
+        (entry)->link.dtq_next = NULL; \
+        (entry)->link.dtq_prev = NULL; \
     } while (0)                        \
 
 
 #define DTQUEUE_ENTRY_FINI DTQUEUE_ENTRY_INIT
 
-#define DTQUEUE_ENQUEUE(q, link, entry)        \
-    do {                                       \
-        if ((q)->tail == NULL) {               \
-            (q)->head = entry;                 \
-            (q)->tail = entry;                 \
-        } else {                               \
-            (q)->tail->link.next = (entry);    \
-            (entry)->link.prev = (q)->tail;    \
-            (entry)->link.next = NULL;         \
-            (q)->tail = (entry);               \
-        }                                      \
-        ++((q)->nelems);                       \
-    } while (0)                                \
+#define DTQUEUE_ENQUEUE(q, link, entry)                \
+    do {                                               \
+        if ((q)->dtq_tail == NULL) {                   \
+            (q)->dtq_head = entry;                     \
+            (q)->dtq_tail = entry;                     \
+        } else {                                       \
+            (q)->dtq_tail->link.dtq_next = (entry);    \
+            (entry)->link.dtq_prev = (q)->dtq_tail;    \
+            (entry)->link.dtq_next = NULL;             \
+            (q)->dtq_tail = (entry);                   \
+        }                                              \
+        ++((q)->nelems);                               \
+    } while (0)                                        \
 
 
-#define DTQUEUE_HEAD(q) ((q)->head)
+#define DTQUEUE_HEAD(q) ((q)->dtq_head)
 
-#define DTQUEUE_NEXT(link, e) ((e)->link.next)
+#define DTQUEUE_NEXT(link, e) ((e)->link.dtq_next)
 
-#define DTQUEUE_PREV(link, e) ((e)->link.prev)
+#define DTQUEUE_PREV(link, e) ((e)->link.dtq_prev)
 
-#define DTQUEUE_TAIL(q) ((q)->tail)
+#define DTQUEUE_TAIL(q) ((q)->dtq_tail)
 
 #define DTQUEUE_LENGTH(q) ((q)->nelems)
 
-#define DTQUEUE_DEQUEUE(q, link)               \
-    do {                                       \
-        if ((q)->head != NULL) {               \
-            PASTEURIZE_ADDR((q)->head);        \
-            (q)->head = (q)->head->link.next;  \
-            PASTEURIZE_ADDR((q)->head);        \
-            if ((q)->head == NULL) {           \
-                (q)->tail = NULL;              \
-            } else {                           \
-                (q)->head->link.prev = NULL;   \
-            }                                  \
-            --((q)->nelems);                   \
-        } else {                               \
-            (q)->tail = NULL;                  \
-        }                                      \
-    } while (0)                                \
+#define DTQUEUE_DEQUEUE(q, link)                               \
+    do {                                                       \
+        if ((q)->dtq_head != NULL) {                           \
+            PASTEURIZE_ADDR((q)->dtq_head);                    \
+            (q)->dtq_head = (q)->dtq_head->link.dtq_next;      \
+            PASTEURIZE_ADDR((q)->dtq_head);                    \
+            if ((q)->dtq_head == NULL) {                       \
+                (q)->dtq_tail = NULL;                          \
+            } else {                                           \
+                (q)->dtq_head->link.dtq_prev = NULL;           \
+            }                                                  \
+            --((q)->nelems);                                   \
+        } else {                                               \
+            (q)->dtq_tail = NULL;                              \
+        }                                                      \
+    } while (0)                                                \
 
 
 /*
@@ -96,86 +96,86 @@ struct {                       \
  *  }
  *
  *  -- right before DTQUEUE_FINI(q). DTQUEUE_DEQUEUE_FAST() leaves a queue
- *  in a state where (head == NULL) AND (tail != NULL). In this state a
+ *  in a state where (dtq_head == NULL) AND (dtq_tail != NULL). In this state a
  *  subsequent DTQUEUE_* operations may lead to memory access error.
  */
-#define DTQUEUE_DEQUEUE_FAST(q, link)          \
-    do {                                       \
-        (q)->head = (q)->head->link.next;      \
-        if ((q)->head != NULL) {               \
-            (q)->head->link.prev = NULL;       \
-        }                                      \
-        --((q)->nelems);                       \
-    } while (0)                                \
+#define DTQUEUE_DEQUEUE_FAST(q, link)                  \
+    do {                                               \
+        (q)->dtq_head = (q)->dtq_head->link.dtq_next;  \
+        if ((q)->dtq_head != NULL) {                   \
+            (q)->dtq_head->link.dtq_prev = NULL;       \
+        }                                              \
+        --((q)->nelems);                               \
+    } while (0)                                        \
 
 
-#define DTQUEUE_REMOVE_DIRTY(q, link, e)                               \
-    do {                                                               \
-        if ((q)->head != NULL && (q)->tail != NULL) {                  \
-            if ((e)->link.prev != NULL) {                              \
-                if ((e)->link.next != NULL) {                          \
-                    ((e)->link.prev)->link.next = (e)->link.next;      \
-                    ((e)->link.next)->link.prev = (e)->link.prev;      \
-                } else {                                               \
-                    (q)->tail = (e)->link.prev;                        \
-                    (q)->tail->link.next = NULL;                       \
-                }                                                      \
-            } else {                                                   \
-                if ((e)->link.next != NULL) {                          \
-                    (q)->head = (e)->link.next;                        \
-                    (q)->head->link.prev = NULL;                       \
-                } else {                                               \
-                    (q)->tail = NULL;                                  \
-                    (q)->head = NULL;                                  \
-                }                                                      \
-            }                                                          \
-            --((q)->nelems);                                           \
-        }                                                              \
-    } while (0)                                                        \
+#define DTQUEUE_REMOVE_DIRTY(q, link, e)                                       \
+    do {                                                                       \
+        if ((q)->dtq_head != NULL && (q)->dtq_tail != NULL) {                  \
+            if ((e)->link.dtq_prev != NULL) {                                  \
+                if ((e)->link.dtq_next != NULL) {                              \
+                    ((e)->link.dtq_prev)->link.dtq_next = (e)->link.dtq_next;  \
+                    ((e)->link.dtq_next)->link.dtq_prev = (e)->link.dtq_prev;  \
+                } else {                                                       \
+                    (q)->dtq_tail = (e)->link.dtq_prev;                        \
+                    (q)->dtq_tail->link.dtq_next = NULL;                       \
+                }                                                              \
+            } else {                                                           \
+                if ((e)->link.dtq_next != NULL) {                              \
+                    (q)->dtq_head = (e)->link.dtq_next;                        \
+                    (q)->dtq_head->link.dtq_prev = NULL;                       \
+                } else {                                                       \
+                    (q)->dtq_tail = NULL;                                      \
+                    (q)->dtq_head = NULL;                                      \
+                }                                                              \
+            }                                                                  \
+            --((q)->nelems);                                                   \
+        }                                                                      \
+    } while (0)                                                                \
 
 
 #define DTQUEUE_REMOVE(q, link, e)                                     \
     DTQUEUE_REMOVE_DIRTY(q, link, e); DTQUEUE_ENTRY_FINI(link, e)      \
 
 
-#define DTQUEUE_ORPHAN(q, link, e) (((q)->head != (e)) && ((e)->link.prev == NULL) && ((e)->link.next == NULL))
+#define DTQUEUE_ORPHAN(q, link, e) (((q)->dtq_head != (e)) && ((e)->link.dtq_prev == NULL) && ((e)->link.dtq_next == NULL))
 
-#define DTQUEUE_EMPTY(q) ((q)->head == NULL && (q)->tail == NULL)
+#define DTQUEUE_EMPTY(q) ((q)->dtq_head == NULL && (q)->dtq_tail == NULL)
 
-#define DTQUEUE_INSERT_AFTER(q, link, a, e)    \
-    do {                                       \
-        assert((a) != NULL && (e) != NULL);    \
-        if ((a)->link.next == NULL) {          \
-            (q)->tail = (e);                   \
-            (e)->link.next = NULL;             \
-            (a)->link.next = (e);              \
-            (e)->link.prev = (a);              \
-        } else {                               \
-            (e)->link.next = (a)->link.next;   \
-            (a)->link.next = (e);              \
-            ((e)->link.next)->link.prev = (e); \
-            (e)->link.prev = (a);              \
-        }                                      \
-        ++((q).nelems);                        \
-    } while (0)                                \
+#define DTQUEUE_INSERT_AFTER(q, link, a, e)            \
+    do {                                               \
+        assert((a) != NULL && (e) != NULL);            \
+        if ((a)->link.dtq_next == NULL) {              \
+            (q)->dtq_tail = (e);                       \
+            (e)->link.dtq_next = NULL;                 \
+            (a)->link.dtq_next = (e);                  \
+            (e)->link.dtq_prev = (a);                  \
+        } else {                                       \
+            (e)->link.dtq_next = (a)->link.dtq_next;   \
+            (a)->link.dtq_next = (e);                  \
+            ((e)->link.dtq_next)->link.dtq_prev = (e); \
+            (e)->link.dtq_prev = (a);                  \
+        }                                              \
+        ++((q).nelems);                                \
+    } while (0)                                        \
 
 
-#define DTQUEUE_INSERT_BEFORE(q, link, b, e)   \
-    do {                                       \
-        assert((b) != NULL && (e) != NULL);    \
-        if ((b)->link.prev == NULL) {          \
-            (q)->head = (e);                   \
-            (e)->link.prev = NULL;             \
-            (b)->link.prev = (e);              \
-            (e)->link.next = (b);              \
-        } else {                               \
-            (e)->link.prev = (b)->link.prev;   \
-            (b)->link.prev = (e);              \
-            ((e)->link.prev)->link.next = (e); \
-            (e)->link.next = (b);              \
-        }                                      \
-        ++((q)->nelems);                       \
-    } while (0)                                \
+#define DTQUEUE_INSERT_BEFORE(q, link, b, e)           \
+    do {                                               \
+        assert((b) != NULL && (e) != NULL);            \
+        if ((b)->link.dtq_prev == NULL) {              \
+            (q)->dtq_head = (e);                       \
+            (e)->link.dtq_prev = NULL;                 \
+            (b)->link.dtq_prev = (e);                  \
+            (e)->link.dtq_next = (b);                  \
+        } else {                                       \
+            (e)->link.dtq_prev = (b)->link.dtq_prev;   \
+            (b)->link.dtq_prev = (e);                  \
+            ((e)->link.dtq_prev)->link.dtq_next = (e); \
+            (e)->link.dtq_next = (b);                  \
+        }                                              \
+        ++((q)->nelems);                               \
+    } while (0)                                        \
 
 
 #ifdef __cplusplus
