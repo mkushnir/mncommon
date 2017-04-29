1abfd075d07cf6cf34accb8d802059322267f5a8
Markiyan Kushnir [at home] <markiyan.kushnir@gmail.com>
2017-04-29 14:14:25 +0300

    added mrkcommon/float.h

---
+++ src/mrkcommon/float.h
@@ -0,0 +1,25 @@
+#ifndef MRKCOMMON_FLOAT_H
+#define MRKCOMMON_FLOAT_H
+
+#include <float.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#define MN_EPSILON FLT_EPSILON
+#define MNDBL_ISZERO(d) (fabs(d) <= MN_EPSILON)
+#define MNDBL_LT(a, b) ((a) < ((b) + MN_EPSILON))
+#define MNDBL_LTE(a, b) ((a) <= ((b) + MN_EPSILON))
+#define MNDBL_GT(a, b) ((a) > ((b) - MN_EPSILON))
+#define MNDBL_GTE(a, b) ((a) >= ((b) - MN_EPSILON))
+#define MNDBL_EQ(a, b)  \
+    (((a) >= ((b) - MN_EPSILON)) && ((a) <= ((b) + MN_EPSILON)))
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
