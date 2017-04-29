40da488d84275ef1449480b0727597b3e0f1bd60
Markiyan Kushnir [at home] <markiyan.kushnir@gmail.com>
2017-04-29 14:17:20 +0300

    fixed lt/gt macros

--- src/mrkcommon/float.h
+++ src/mrkcommon/float.h
@@ -10,9 +10,9 @@ extern "C" {
 
 #define MN_EPSILON FLT_EPSILON
 #define MNDBL_ISZERO(d) (fabs(d) <= MN_EPSILON)
-#define MNDBL_LT(a, b) ((a) < ((b) + MN_EPSILON))
+#define MNDBL_LT(a, b) ((a) < ((b) - MN_EPSILON))
 #define MNDBL_LTE(a, b) ((a) <= ((b) + MN_EPSILON))
-#define MNDBL_GT(a, b) ((a) > ((b) - MN_EPSILON))
+#define MNDBL_GT(a, b) ((a) > ((b) + MN_EPSILON))
 #define MNDBL_GTE(a, b) ((a) >= ((b) - MN_EPSILON))
 #define MNDBL_EQ(a, b)  \
     (((a) >= ((b) - MN_EPSILON)) && ((a) <= ((b) + MN_EPSILON)))
