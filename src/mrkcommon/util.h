bd7f36b4329d6e10e1b841c6f0ede3e931061612
Markiyan Kushnir [at home] <markiyan.kushnir@gmail.com>
2017-07-28 15:55:26 +0300

    wider MRKASZ() macro, more tests

--- src/mrkcommon/util.h
+++ src/mrkcommon/util.h
@@ -172,9 +172,13 @@ _DMIN(intmax_t, mnmin_intmin)
 #   define OUT
 #endif
 
-#define MRKMAXASZ (16)
-#define MRKCNT16(_, a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, ...) a16
-#define MRKASZ(...) MRKCNT16(, ## __VA_ARGS__, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+//#define MRKMAXASZ (16)
+//#define MRKCNT16(_, a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, ...) a16
+//#define MRKASZ(...) MRKCNT16(, ## __VA_ARGS__, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+
+#define MRKMAXASZ (32)
+#define MRKCNT32(_, a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, ...) a32
+#define MRKASZ(...) MRKCNT32(, ## __VA_ARGS__, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
 
 #define INB0(lo, x, hi) (((lo) <= (x)) && ((x) <= (hi)))
 #define INB1(lo, x, hi) (((lo) <= (x)) && ((x) < (hi)))
---
+++ test/testdtqueueperf.c
@@ -0,0 +1,179 @@
+#include <assert.h>
+#include <err.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "unittest.h"
+#include "diag.h"
+#include <mrkcommon/dumpm.h>
+#include <mrkcommon/util.h>
+#include <mrkcommon/dtqueue.h>
+
+
+#ifndef NDEBUG
+#define NGX_DEBUG 1
+#endif
+
+
+
+/*
+ * Copyright (C) Igor Sysoev
+ * Copyright (C) Nginx, Inc.
+ */
+typedef struct ngx_queue_s  ngx_queue_t;
+
+struct ngx_queue_s {
+    ngx_queue_t  *prev;
+    ngx_queue_t  *next;
+};
+
+
+#define ngx_queue_init(q)                                                     \
+    (q)->prev = q;                                                            \
+    (q)->next = q
+
+
+#define ngx_queue_empty(h)                                                    \
+    (h == (h)->prev)
+
+
+#define ngx_queue_insert_head(h, x)                                           \
+    (x)->next = (h)->next;                                                    \
+    (x)->next->prev = x;                                                      \
+    (x)->prev = h;                                                            \
+    (h)->next = x
+
+
+#define ngx_queue_insert_after   ngx_queue_insert_head
+
+
+#define ngx_queue_insert_tail(h, x)                                           \
+    (x)->prev = (h)->prev;                                                    \
+    (x)->prev->next = x;                                                      \
+    (x)->next = h;                                                            \
+    (h)->prev = x
+
+
+#define ngx_queue_head(h)                                                     \
+    (h)->next
+
+
+#define ngx_queue_last(h)                                                     \
+    (h)->prev
+
+
+#define ngx_queue_sentinel(h)                                                 \
+    (h)
+
+
+#define ngx_queue_next(q)                                                     \
+    (q)->next
+
+
+#define ngx_queue_prev(q)                                                     \
+    (q)->prev
+
+
+#if (NGX_DEBUG)
+
+#define ngx_queue_remove(x)                                                   \
+    (x)->next->prev = (x)->prev;                                              \
+    (x)->prev->next = (x)->next;                                              \
+    (x)->prev = NULL;                                                         \
+    (x)->next = NULL
+
+#else
+
+#define ngx_queue_remove(x)                                                   \
+    (x)->next->prev = (x)->prev;                                              \
+    (x)->prev->next = (x)->next
+
+#endif
+
+
+#define ngx_queue_split(h, q, n)                                              \
+    (n)->prev = (h)->prev;                                                    \
+    (n)->prev->next = n;                                                      \
+    (n)->next = q;                                                            \
+    (h)->prev = (q)->prev;                                                    \
+    (h)->prev->next = h;                                                      \
+    (q)->prev = n;
+
+
+#define ngx_queue_add(h, n)                                                   \
+    (h)->prev->next = (n)->next;                                              \
+    (n)->next->prev = (h)->prev;                                              \
+    (h)->prev = (n)->prev;                                                    \
+    (h)->prev->next = h;
+
+
+#define ngx_queue_data(q, type, link)                                         \
+    (type *) ((u_char *) q - offsetof(type, link))
+
+
+
+
+
+/*
+ *
+ */
+
+typedef struct _mnitem {
+    ngx_queue_t ngx_link;
+    DTQUEUE_ENTRY(_mnitem, mrk_link);
+} mnitem_t;
+
+DTQUEUE(_mnitem, mrkq) = DTQUEUE_INITIALIZER;
+ngx_queue_t ngxq;
+
+
+#define N 100000000
+
+int
+main(int argc, char **argv)
+{
+    mnitem_t *items;
+    size_t i;
+
+    //srandom(time(NULL));
+
+    DTQUEUE_INIT(&mrkq);
+    ngx_queue_init(&ngxq);
+
+    if ((items = malloc(sizeof(mnitem_t) * N)) == NULL) {
+        errx(1, "malloc");
+    }
+
+    if (argc == 2) {
+        if (strcmp(argv[1], "ngx") == 0) {
+            for (i = 0; i < N; ++i) {
+                ngx_queue_init(&(items + i)->ngx_link);
+            }
+            for (i = 0; i < N; ++i) {
+                ngx_queue_insert_tail(&ngxq, &(items + i)->ngx_link);
+            }
+            while (!ngx_queue_empty(&ngxq)) {
+                ngx_queue_t *qe;
+                qe = ngx_queue_head(&ngxq);
+                ngx_queue_remove(qe);
+            }
+        } else if (strcmp(argv[1], "mrk") == 0) {
+            for (i = 0; i < N; ++i) {
+                DTQUEUE_ENTRY_INIT(mrk_link, items + i);
+            }
+            for (i = 0; i < N; ++i) {
+                DTQUEUE_ENQUEUE(&mrkq, mrk_link, items + i);
+            }
+            while (!DTQUEUE_EMPTY(&mrkq)) {
+                mnitem_t *item;
+                item = DTQUEUE_HEAD(&mrkq);
+                DTQUEUE_DEQUEUE(&mrkq, mrk_link);
+            }
+        } else {
+            errx(1, "arg can be one of ngx/mrk\n");
+        }
+    }
+
+
+    return 0;
+}
