90ad8141c11feea5832b08273d1ad9d4fcde799e
Markiyan Kushnir [at home] <markiyan.kushnir@gmail.com>
2024-11-18 09:53:30 +0000

    typo

--- src/mncommon/time.h
+++ src/mncommon/time.h
@@ -46,7 +46,7 @@ extern "C" {
 #define MN_TIMESPEC_DIFF2(res, a, b)           \
 {                                              \
     (res).tv_sec = (a).tv_sec - (b).tv_sec;    \
-    (res).tv_nsec = (a).tv_usec - (b).tv_nsec; \
+    (res).tv_nsec = (a).tv_nsec - (b).tv_nsec; \
                                                \
     if ((res).tv_nsec < 0) {                   \
         --(res).tv_sec;                        \
